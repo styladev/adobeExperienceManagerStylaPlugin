223df4c7970ad633467bb08abd43c0a1d576156f
ssachtleben <ssachtleben@gmail.com>
2020-02-07 09:03:17 +0100

    bumped version

--- core/src/main/java/de/neofonie/styla/core/package-info.java
+++ core/src/main/java/de/neofonie/styla/core/package-info.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-@Version("1.1.4")
+@Version("1.1.5")
 package de.neofonie.styla.core;
 
 import org.osgi.annotation.versioning.Version;
